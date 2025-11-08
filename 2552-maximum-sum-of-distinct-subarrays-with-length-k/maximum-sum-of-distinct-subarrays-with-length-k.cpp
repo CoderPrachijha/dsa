d6e3b41e7268fddc6f6eef6be9e1e1579b90afc3
PRACHI JHA <179294335+CoderPrachijha@users.noreply.github.com>
2025-11-09 01:17:40 +0530

    Time: 103 ms (58.82%) | Memory: 95.1 MB (93.33%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -5,19 +5,15 @@ public:
         unordered_map<int,int> freq; 
         long long sum = 0, res = 0;
         int i = 0;
-        
         for(int j= 0; j < n; j++) {
             freq[arr[j]]++;
             sum += arr[j];
-            
-           
             if(j - i + 1 > k) {
                 sum -= arr[i];
                 freq[arr[i]]--;
                 if(freq[arr[i]] == 0) freq.erase(arr[i]);
                 i++;
             }
-            
             if(j - i + 1 == k && freq.size() == k) {
                 res = max(res, sum);
             }
