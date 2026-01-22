d6352c92ddd07db88c8a03d652a921d6806b46a4
PRACHI JHA <179294335+CoderPrachijha@users.noreply.github.com>
2026-01-22 09:07:59 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (39.60%) - LeetSync

---
+++ 43-multiply-strings/multiply-strings.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        if(num1=="0" || num2=="0") return "0";
+        int n=num1.size();
+        int m=num2.size();
+
+        int res[500]={0};
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                int mul=(num1[i]-'0')* (num2[j]-'0');
+                int sum=mul+ res[i+j+1];
+                res[i+j+1]=sum%10;
+                res[i+j]+=sum/10;
+
+            }
+        }
+        string ans = "";
+        int i = 0;
+        while (i < n + m && res[i] == 0) i++;
+
+        for (; i < n + m; i++)
+            ans += res[i] + '0';
+
+        return ans;
+
+        
+    }
+};
